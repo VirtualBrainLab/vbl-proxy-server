e84e59997adead387853e863d85e67d3eebf978a
Kenneth Yang <kjy5@uw.edu>
2024-04-24 11:58:46 -0700

    Also send requester and define pair

--- index.ts
+++ index.ts
@@ -3,21 +3,26 @@ import {Server} from "socket.io";
 const io = new Server(3000);
 console.log("Server started");
 
+type RequesterResponderPair = {
+    requesterSid: string,
+    responderSid: string,
+}
+
 io.on("connection", (socket) => {
     // Connection and request ID
     console.log(`Service connected: ${socket.id}`);
-    socket.emit("get_id", (response: string) => {
-        console.log(response);
+    socket.emit("get_id", (response: string, isRequester: boolean) => {
+        console.log(response + " " + isRequester);
     });
-    
-    
+
+
     socket.on("disconnect", () => {
         console.log(`Service disconnected: ${socket.id}`);
     });
-    
+
     socket.on("poke", (data) => {
         console.log(data);
-    
+
     })
 
     socket.onAny((event, args, callback) => {
