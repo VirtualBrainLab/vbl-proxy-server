487d8f3444dc76b3b2d5d20ac107a23637afd67d
Kenneth Yang <kjy5@uw.edu>
2024-04-24 14:04:25 -0700

    Record connection

--- index.ts
+++ index.ts
@@ -1,21 +1,43 @@
 import {Server} from "socket.io";
 
+// Create server.
 const io = new Server(3000);
 console.log("Server started");
 
+// Record connections.
 type RequesterResponderPair = {
     requesterSid: string,
     responderSid: string,
 }
+let connections: { [id: string]: RequesterResponderPair } = {};
 
+// Connection handler.
 io.on("connection", (socket) => {
-    // Connection and request ID
     console.log(`Service connected: ${socket.id}`);
-    socket.emit("get_id", (response: string, isRequester: boolean) => {
-        console.log(response + " " + isRequester);
+
+    // Local properties
+    let pinpointId: string = "";
+    let isRequester: boolean = false;
+
+    // Record ID.
+    socket.emit("get_pinpoint_id", (pinpointIdInput: string, isRequesterInput: boolean) => {
+        console.log("Socket: " + socket.id + " is a " + (isRequesterInput ? "Requester" : "Responder") + " in pinpoint link " + pinpointIdInput);
+        pinpointId = pinpointIdInput;
+        isRequester = isRequesterInput;
+
+        // Add connection if new.
+        if (!(pinpointIdInput in connections)) {
+            connections[pinpointIdInput] = {requesterSid: "", responderSid: ""};
+        }
+        // Set the socket ID to the role type.
+        if (isRequesterInput) {
+            connections[pinpointIdInput].requesterSid = socket.id;
+        } else {
+            connections[pinpointIdInput].responderSid = socket.id;
+        }
     });
 
-
+    // Disconnect handler.
     socket.on("disconnect", () => {
         console.log(`Service disconnected: ${socket.id}`);
     });
